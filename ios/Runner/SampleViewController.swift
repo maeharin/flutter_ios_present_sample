1cafa95d7cd4b4d3ae641415d8b8b66552b5509c
maeharin <maeharin@gmail.com>
2019-07-15 14:55:11 +0900

    flutter 1.5.4-hotfix.2

--- ios/Runner/SampleViewController.swift
+++ ios/Runner/SampleViewController.swift
@@ -13,5 +13,22 @@ class SampleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        
+        let closeButton = UIButton()
+        closeButton.setTitle("close", for: .normal)
+        closeButton.sizeToFit()
+        closeButton.addTarget(self, action: #selector(onCloseButtonTapped(sender:)), for: .touchUpInside)
+        view.addSubview(closeButton)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            closeButton.widthAnchor.constraint(equalToConstant: 100),
+            closeButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
+    }
+    
+    @objc func onCloseButtonTapped(sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 }
